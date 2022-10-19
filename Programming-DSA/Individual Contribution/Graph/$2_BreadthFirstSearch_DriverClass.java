23249386dd34bba2c72ee5ee7c537018763dcd8e
Wakanda Rajyam <116173131+wakandarajyam@users.noreply.github.com>
2022-10-19 20:13:31 +0530

    Organised into folders, Improved Security and Readability

--- Programming-DSA/Individual Contribution/Graph/$2_BreadthFirstSearch_DriverClass.java
+++ Programming-DSA/Individual Contribution/Graph/$2_BreadthFirstSearch_DriverClass.java
@@ -1,23 +1,25 @@
 public class $2_BreadthFirstSearch_DriverClass {
-    public static void main(String[] args) {
-        int[][] arr={
-            {0,1,1,0,1,0,0},
-            {1,0,0,1,0,0,0},
-            {1,0,0,1,0,0,0},
-            {0,1,1,0,1,1,1},
-            {1,0,0,1,0,0,0},
-            {0,0,0,1,0,0,0},
-            {0,0,0,1,0,0,0}
-        };
-        $1_Graph graph=new $1_Graph(arr);
+	public static void main(String[] args) {
+		
+        int[][] arr = { 
+				{ 0, 1, 1, 0, 1, 0, 0 }, 
+				{ 1, 0, 0, 1, 0, 0, 0 }, 
+				{ 1, 0, 0, 1, 0, 0, 0 },
+				{ 0, 1, 1, 0, 1, 1, 1 }, 
+				{ 1, 0, 0, 1, 0, 0, 0 }, 
+				{ 0, 0, 0, 1, 0, 0, 0 }, 
+				{ 0, 0, 0, 1, 0, 0, 0 } 
+		};
 
-        System.out.println("Breadth First Search: ");
-        graph.breadthFirstSearch(1);
-        graph.breadthFirstSearch(2);
-        graph.breadthFirstSearch(3);
-        graph.breadthFirstSearch(4);
-        graph.breadthFirstSearch(5);
-        graph.breadthFirstSearch(6);
-        graph.breadthFirstSearch(7);
-    }
+		$1_Graph graph = new $1_Graph(arr);
+
+		System.out.println("Breadth First Search: ");
+		graph.breadthFirstSearch(1);
+		graph.breadthFirstSearch(2);
+		graph.breadthFirstSearch(3);
+		graph.breadthFirstSearch(4);
+		graph.breadthFirstSearch(5);
+		graph.breadthFirstSearch(6);
+		graph.breadthFirstSearch(7);
+	}
 }
--- Programming-DSA/Individual Contribution/Sorting/CocktailSort.java
+++ Programming-DSA/Individual Contribution/Sorting/CocktailSort.java
@@ -1,14 +1,12 @@
 // Java program for implementation of Cocktail Sort
-public class CocktailSort
-{
-	void cocktailSort(int a[])
-	{
+
+public class CocktailSort {
+	void cocktailSort(int a[]) {
 		boolean swapped = true;
 		int start = 0;
 		int end = a.length;
 
-		while (swapped == true)
-		{
+		while (swapped == true) {
 			// reset the swapped flag on entering the
 			// loop, because it might be true from a
 			// previous iteration.
@@ -16,8 +14,7 @@ public class CocktailSort
 
 			// loop from bottom to top same as
 			// the bubble sort
-			for (int i = start; i < end - 1; ++i)
-			{
+			for (int i = start; i < end - 1; ++i) {
 				if (a[i] > a[i + 1]) {
 					int temp = a[i];
 					a[i] = a[i + 1];
@@ -40,10 +37,8 @@ public class CocktailSort
 
 			// from top to bottom, doing the
 			// same comparison as in the previous stage
-			for (int i = end - 1; i >= start; i--)
-			{
-				if (a[i] > a[i + 1])
-				{
+			for (int i = end - 1; i >= start; i--) {
+				if (a[i] > a[i + 1]) {
 					int temp = a[i];
 					a[i] = a[i + 1];
 					a[i + 1] = temp;
@@ -59,8 +54,7 @@ public class CocktailSort
 	}
 
 	/* Prints the array */
-	void printArray(int a[])
-	{
+	void printArray(int a[]) {
 		int n = a.length;
 		for (int i = 0; i < n; i++)
 			System.out.print(a[i] + " ");
@@ -68,8 +62,7 @@ public class CocktailSort
 	}
 
 	// Driver code
-	public static void main(String[] args)
-	{
+	public static void main(String[] args) {
 		CocktailSort ob = new CocktailSort();
 		int a[] = { 5, 1, 4, 2, 8, 0, 2 };
 		ob.cocktailSort(a);
